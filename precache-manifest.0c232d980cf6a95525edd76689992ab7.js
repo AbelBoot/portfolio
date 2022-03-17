19c070e9f1e0880d93708de89430c012b30167a6
Abel <abellocati@homail.com>
2022-03-17 15:44:18 +0000

    Updates

---
+++ precache-manifest.0c232d980cf6a95525edd76689992ab7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca25d0bab4e7fd1c201afb4237784e7a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9e422cee3f1e765f309b",
+    "url": "/portfolio/static/css/main.626ac9ae.chunk.css"
+  },
+  {
+    "revision": "d9d710c59fed9c011bb0",
+    "url": "/portfolio/static/js/2.cef8457d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.cef8457d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e422cee3f1e765f309b",
+    "url": "/portfolio/static/js/main.6b9926e7.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
